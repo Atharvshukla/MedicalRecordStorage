f9b2ec9e4e0d835b81f9971fb57f5e2d62f2d166
Atharv Shukla <38507802+Atharvshukla@users.noreply.github.com>
2024-03-21 16:49:01 +0530

    frontend updated

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -51,7 +51,7 @@ const Form = () => {
           value={name === 0 ? "" : name}
           onChange={(e) => setName(e.target.value)}
           required
-          placeholder="Aman Dhattarwal"
+          placeholder="Atharv Shukla"
         />
 
         <label htmlFor="age">Age:</label>
